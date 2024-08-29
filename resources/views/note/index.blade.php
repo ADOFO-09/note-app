37350af76ed791685c05b509b03ae4c70a27199f
ADOFO-09 <emmanueladofo011@gmail.com>
2024-08-29 04:44:23 +0000

    Rendering note content into index file

--- resources/views/note/index.blade.php
+++ resources/views/note/index.blade.php
@@ -1,4 +1,23 @@
 <x-layout>
     <!-- Smile, breathe, and go slowly. - Thich Nhat Hanh -->
-    <h1>index</h1>
+    <div class="note-container">
+        <a href="{{route('note.create')}}" class="new-note-btn">
+            New Note
+        </a>
+        <div class="notes">
+            @foreach ($notes as $note)
+                <div class="note">
+                    <div class="note-body">
+                        {{Str::words($note->note, 30)}}
+                    </div>
+                    <div class="note-buttons">
+                        <a href="{{route('note.show', ['note' => $note])}}" class="note-edit-button">View</a>
+                        <a href="{{route('note.edit', ['note' => $note])}}" class="note-edit-button">Edit</a>
+                        <button class="note-delete-button">Delete</button>
+                    </div>
+                </div>    
+            @endforeach
+            
+        </div>
+    </div>
 </x-layout>
