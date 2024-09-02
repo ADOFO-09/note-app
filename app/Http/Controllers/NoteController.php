4ef474bd7affe2e210df481974b54ee5609306bc
ADOFO-09 <emmanueladofo011@gmail.com>
2024-09-02 04:59:02 +0000

    Note create

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -29,7 +29,14 @@ class NoteController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = $request->validate([
+            'note' => ['required','string']
+        ]);
+
+        $data['user_id'] = 1;
+        $note = Note::create($data);
+
+        return to_route('note.show', ['note' => $note])->with('message', 'Note was created successfully');
     }
 
     /**
