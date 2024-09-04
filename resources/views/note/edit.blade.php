a3578ebab1febed4c3590cc3990741f5340c154e
ADOFO-09 <emmanueladofo011@gmail.com>
2024-09-04 04:43:29 +0000

    Final touches on appearance

--- resources/views/note/edit.blade.php
+++ resources/views/note/edit.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <div class="note-container single-note">
-        <h1>Edit your note</h1>
+        <h1 class="text-3xl py-4">Edit your note</h1>
         <form action="{{route('note.update', ['note' => $note])}}" method="post" class="note">
             @csrf
             @method('PUT');
--- resources/views/note/index.blade.php
+++ resources/views/note/index.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <!-- Smile, breathe, and go slowly. - Thich Nhat Hanh -->
-    <div class="note-container">
+    <div class="note-container py-12">
         <a href="{{route('note.create')}}" class="new-note-btn">
             New Note
         </a>
@@ -23,6 +23,8 @@
                 </div>    
             @endforeach
         </div>
-        {{$notes->links()}}
+        <div class="p-6">
+            {{$notes->links()}}
+        </div>
     </div>
 </x-app-layout>
